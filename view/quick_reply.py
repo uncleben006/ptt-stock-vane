72dc461c1388ad636158f785b80d480cccf29a89
ben_wang <uncleben006@gmail.com>
2020-07-13 18:06:07 +0800

    Update quick reply

--- view/quick_reply.py
+++ view/quick_reply.py
@@ -13,7 +13,7 @@ def quick_reply():
         items=[
             # QuickReplyButton(action=DatetimePickerAction( label="快速查詢風向", data="stock_company_list", mode="date", initial=str(today), max=str(today), min=str(today - timedelta(days=30)) ) ),
             QuickReplyButton(action=PostbackAction( label="快速查詢風向", data="stock_company_list" ) ),
-            QuickReplyButton(action=MessageAction(label="意見領袖報酬率", text="查詢公司名稱或代號")),
+            QuickReplyButton(action=PostbackAction(label="意見領袖報酬率", text="stock_company_list")),
             QuickReplyButton(action=MessageAction(label="指令", text='指令')),
         ]
     )
